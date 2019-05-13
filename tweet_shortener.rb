534b7434ef5bb3bf7e5c1ab84c3299f68c137330
Jasmine Chen <jasmine1226@gmail.com>
2019-05-13 05:49:50 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+
+
+def dictionary(word)
+  dic = {
+    hello: 'hi',
+    to: '2',
+    two: '2',
+    too: '2',
+    for: '4',
+    four: '4',
+    be: 'b',
+    you: 'u',
+    at: '@',
+    and: '&'
+  }
+
+  dic.each do |key, value|
+    puts "#{key}: #{value}"
+    if key == word
+      word = dic[key]
+      puts dic[key]
+      puts word
+    end
+  end
+  puts word
+end
+
+dictionary("you")
+dictionary("apple")
+
+def word_substituter
+
+end
